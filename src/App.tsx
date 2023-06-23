28fe047896ae27a3f7ba65c6a8f6c67af21630bf
weikang <weikang.ng@evdg.cc>
2023-06-23 17:21:07 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -15,60 +15,18 @@ import TradeFair from "./pages/New/TradeFair";
 import TyreTest from "./pages/New/TyreTest";
 import CompanyUpdate from "./pages/New/CompanyUpdate";
 import SubContact from "./component/SubMenu/SubContact";
-import NewDetails_19AUG21 from "./pages/New/NewDetails/NewDetails_19AUG21";
-import NewDetails_17MAY21 from "./pages/New/NewDetails/NewDetails_17MAY21";
-import NewDetails_03FEB21 from "./pages/New/NewDetails/NewDetails_03FEB21";
-import NewDetails_20JAN21 from "./pages/New/NewDetails/NewDetails_20JAN21";
-import NewDetails_11JAN21 from "./pages/New/NewDetails/NewDetails_11JAN21";
-import NewDetails_30DEC20 from "./pages/New/NewDetails/NewDetails_30DEC20";
-import NewDetails_30NOV20 from "./pages/New/NewDetails/NewDetails_30NOV20";
-import NewDetails_10NOV20 from "./pages/New/NewDetails/NewDetails_10NOV20";
-import NewDetails_01SEP20 from "./pages/New/NewDetails/NewDetails_01SEP20_2";
-import NewDetails_01SEP20_2 from "./pages/New/NewDetails/NewDetails_01SEP20_2";
+import NewDetailsRoutes from "./NewDetailsRoutes";
 
 function App() {
-  const newDetailsRoutes = [
-    { path: "385-65r22-5-new-pattern-hf281", component: NewDetails_19AUG21 },
-    {
-      path: "roadone-12-00r20-new-truck-tire-road-test",
-      component: NewDetails_17MAY21,
-    },
-    { path: "295-80r22-5-hf21-truck-tire-test", component: NewDetails_03FEB21 },
-    {
-      path: "xcmg-feedback-award-to-tongli-tire-co-ltd",
-      component: NewDetails_20JAN21,
-    },
-    {
-      path: "hixih-continental-cooperation",
-      component: NewDetails_11JAN21,
-    },
-    {
-      path: "hixih-steel-wire-rope-co-ltd",
-      component: NewDetails_30DEC20,
-    },
-    {
-      path: "hd520-pattern-road-test",
-      component: NewDetails_30NOV20,
-    },
-    {
-      path: "14-00r20-special-off-road-tyres",
-      component: NewDetails_10NOV20,
-    },
-    {
-      path: "roadone-becomes-good-supplier-of-the-china-famous-truck-company",
-      component: NewDetails_01SEP20,
-    },
-    {
-      path: "roadone-m665a-heavy-duty-13r22.5-test",
-      component: NewDetails_01SEP20_2,
-    },
-  ].map(({ path, component }) => (
-    <Route
-      key={path}
-      path={`/news/${path}`}
-      element={React.createElement(component)}
-    />
-  ));
+  const renderedNewDetailsRoutes = NewDetailsRoutes.map(
+    ({ path, component }) => (
+      <Route
+        key={path}
+        path={`/news/${path}`}
+        element={React.createElement(component)}
+      />
+    )
+  );
   return (
     <>
       <Router>
@@ -81,8 +39,8 @@ function App() {
           <Route path="/news/newproducts" element={<NewProducts />} />
           <Route path="/news/tradefair" element={<TradeFair />} />
           <Route path="/news/tyretest" element={<TyreTest />} />
-          <Route path="/news/companyupdate" element={<CompanyUpdate />} />s{" "}
-          {newDetailsRoutes}
+          <Route path="/news/companyupdate" element={<CompanyUpdate />} />
+          {renderedNewDetailsRoutes}
           <Route path="/services" element={<Servicepage />} />
           <Route path="/contact" element={<Contactpage />} />
         </Routes>
---
+++ src/NewDetailsRoutes.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import NewDetails_19AUG21 from "./pages/New/NewDetails/NewDetails_19AUG21";
+import NewDetails_17MAY21 from "./pages/New/NewDetails/NewDetails_17MAY21";
+import NewDetails_03FEB21 from "./pages/New/NewDetails/NewDetails_03FEB21";
+import NewDetails_20JAN21 from "./pages/New/NewDetails/NewDetails_20JAN21";
+import NewDetails_11JAN21 from "./pages/New/NewDetails/NewDetails_11JAN21";
+import NewDetails_30DEC20 from "./pages/New/NewDetails/NewDetails_30DEC20";
+import NewDetails_30NOV20 from "./pages/New/NewDetails/NewDetails_30NOV20";
+import NewDetails_10NOV20 from "./pages/New/NewDetails/NewDetails_10NOV20";
+import NewDetails_01SEP20 from "./pages/New/NewDetails/NewDetails_01SEP20_2";
+import NewDetails_01SEP20_2 from "./pages/New/NewDetails/NewDetails_01SEP20_2";
+
+const NewDetailsRoutes = [
+  { path: "385-65r22-5-new-pattern-hf281", component: NewDetails_19AUG21 },
+  {
+    path: "roadone-12-00r20-new-truck-tire-road-test",
+    component: NewDetails_17MAY21,
+  },
+  { path: "295-80r22-5-hf21-truck-tire-test", component: NewDetails_03FEB21 },
+  {
+    path: "xcmg-feedback-award-to-tongli-tire-co-ltd",
+    component: NewDetails_20JAN21,
+  },
+  {
+    path: "hixih-continental-cooperation",
+    component: NewDetails_11JAN21,
+  },
+  {
+    path: "hixih-steel-wire-rope-co-ltd",
+    component: NewDetails_30DEC20,
+  },
+  {
+    path: "hd520-pattern-road-test",
+    component: NewDetails_30NOV20,
+  },
+  {
+    path: "14-00r20-special-off-road-tyres",
+    component: NewDetails_10NOV20,
+  },
+  {
+    path: "roadone-becomes-good-supplier-of-the-china-famous-truck-company",
+    component: NewDetails_01SEP20,
+  },
+  {
+    path: "roadone-m665a-heavy-duty-13r22.5-test",
+    component: NewDetails_01SEP20_2,
+  },
+];
+
+export default NewDetailsRoutes;
