dd54fa9d91da5e2760e010aec7f444559a8b2dbc
weikang <weikang.ng@evdg.cc>
2023-07-17 14:18:12 +0800

    update

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, "build")));
+
+// Handle all other routes by returning the main index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
