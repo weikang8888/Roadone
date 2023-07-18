70fe2a8b397e48e7faa9eaa77b1bf675d0fb5d1d
weikang <weikang.ng@evdg.cc>
2023-07-18 16:25:30 +0800

    update

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -6,12 +6,8 @@ import Logo from "../../static/image/main/logo.png";
 
 const Header = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
-  const [isSubMenuOpen, setSubMenuOpen] = useState(false); // Added state variable for submenu
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
-  };
-  const toggleSubMenu = () => {
-    setSubMenuOpen(!isSubMenuOpen);
   };
   const { t, i18n } = useTranslation();
   const changeLanguage = (language) => {
@@ -35,17 +31,18 @@ const Header = () => {
               </Link>
             </div>
             <div>
-              <ul className="menu">
-                <li>
-                  <NavLink to="/" className="inmenu">
-                    {t("header.home")}
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/products" className="inmenu">
-                    {t("header.product")}
-                  </NavLink>
-                  {/* <ul className="submenu nav0 ">
+              {isMenuOpen && (
+                <ul className="menu">
+                  <li>
+                    <NavLink to="/" className="inmenu">
+                      {t("header.home")}
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/products" className="inmenu">
+                      {t("header.product")}
+                    </NavLink>
+                    {/* <ul className="submenu nav0 ">
                     <li className="side_nav1" id="HeadProCat1">
                       <Link to="">Truck Tyre</Link>
                     </li>
@@ -62,73 +59,74 @@ const Header = () => {
                       <Link to="">ROADONE TYRE DEMONSTRATION</Link>
                     </li>
                   </ul> */}
-                </li>
-                <li>
-                  <NavLink to="/about" className="inmenu">
-                    {t("header.about")}
-                  </NavLink>
-                </li>
-                <li id="linewslist">
-                  <NavLink to="/news" className="inmenu">
-                    {t("header.news")}
-                  </NavLink>
-                  <ul className={`submenu ${isSubMenuOpen ? "d-block" : ""}`}>
-                    <li id="HeadNewsCat1">
-                      <Link to="/news/newproducts" title="New products">
-                        {t("header.new-products")}
-                      </Link>
-                    </li>
-                    <li id="HeadNewsCat2">
-                      <Link to="/news/tradefair" title="Trade Fair">
-                        {t("header.trade-fair")}
-                      </Link>
-                    </li>
-                    <li id="HeadNewsCat3">
-                      <Link to="/news/tyretest" title="TYRE TEST">
-                        {t("header.tyre-test")}
-                      </Link>
-                    </li>
-                    <li id="HeadNewsCat4">
-                      <Link to="/news/companyupdate" title="Company update">
-                        {t("header.company-update")}
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-                <li id="liInfo">
-                  <NavLink to="/services" className="inmenu">
-                    {t("header.service")}
-                  </NavLink>
-                  <ul className={`submenu ${isSubMenuOpen ? "d-block" : ""}`}>
-                    <li id="HeadIndustryCat1">
-                      <Link to="/services/companynews" title="Company News">
-                        {t("header.company-news")}
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <NavLink to="/contact" className="inmenu">
-                    {t("header.contact")}
-                  </NavLink>
-                </li>
-                {/* <li id="lidownload">
+                  </li>
+                  <li>
+                    <NavLink to="/about" className="inmenu">
+                      {t("header.about")}
+                    </NavLink>
+                  </li>
+                  <li id="linewslist">
+                    <NavLink to="/news" className="inmenu">
+                      {t("header.news")}
+                    </NavLink>
+                    <ul className={"submenu"}>
+                      <li id="HeadNewsCat1">
+                        <Link to="/news/newproducts" title="New products">
+                          {t("header.new-products")}
+                        </Link>
+                      </li>
+                      <li id="HeadNewsCat2">
+                        <Link to="/news/tradefair" title="Trade Fair">
+                          {t("header.trade-fair")}
+                        </Link>
+                      </li>
+                      <li id="HeadNewsCat3">
+                        <Link to="/news/tyretest" title="TYRE TEST">
+                          {t("header.tyre-test")}
+                        </Link>
+                      </li>
+                      <li id="HeadNewsCat4">
+                        <Link to="/news/companyupdate" title="Company update">
+                          {t("header.company-update")}
+                        </Link>
+                      </li>
+                    </ul>
+                  </li>
+                  <li id="liInfo">
+                    <NavLink to="/services" className="inmenu">
+                      {t("header.service")}
+                    </NavLink>
+                    <ul className={"submenu"}>
+                      <li id="HeadIndustryCat1">
+                        <Link to="/services/companynews" title="Company News">
+                          {t("header.company-news")}
+                        </Link>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <NavLink to="/contact" className="inmenu">
+                      {t("header.contact")}
+                    </NavLink>
+                  </li>
+                  {/* <li id="lidownload">
                   <NavLink to="/download" className="inmenu">
                     {t("header.download")}
                   </NavLink>
                 </li> */}
-                <li id="liInfo">
-                  <a className="inmenu"> {t("header.language")} </a>
-                  <ul className={`submenu ${isSubMenuOpen ? "d-block" : ""}`}>
-                    <li onClick={changeToEnglish} className="laugage_cursor">
-                      <a> {t("header.english")}</a>
-                    </li>
-                    <li onClick={changeToChinese} className="laugage_cursor">
-                      <a>{t("header.chinese")}</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
+                  <li id="liInfo">
+                    <a className="inmenu"> {t("header.language")} </a>
+                    <ul className={"submenu"}>
+                      <li onClick={changeToEnglish} className="laugage_cursor">
+                        <a> {t("header.english")}</a>
+                      </li>
+                      <li onClick={changeToChinese} className="laugage_cursor">
+                        <a>{t("header.chinese")}</a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              )}
             </div>
             <span className="fr nbtn" onClick={toggleMenu}>
               Menu
