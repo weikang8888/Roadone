115bf0e6c4e8da406374d97284917c27325f06b6
weikang <weikang.ng@evdg.cc>
2023-08-04 14:23:03 +0800

    delete previous roadone

--- src/pages/New/NewDetails/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from "./NewDetails_19AUG21";
-export * from "./NewDetails_17MAY21";
-export * from "./NewDetails_03FEB21";
-export * from "./NewDetails_20JAN21";
-export * from "./NewDetails_21AUG20_2";
-
