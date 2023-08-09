929c46247ded3bd06d562104bad47fc128764e94
weikang <weikang.ng@evdg.cc>
2023-08-09 08:49:03 +0800

    update homepage mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./static/css/style.css";
 import "./static/css/bootstrap.min.css";
+import "./static/css/style.css";
 import Homepage from "./pages/Homepage/Homepage";
 import Newpage from "./pages/New/Newpage";
 import Footer from "./component/Footer/Footer";
@@ -22,6 +22,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
+
         <Route path="/news" element={<Newpage />} />
         <Route path="/services" element={<Servicepage />} />
         <Route
--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -57,7 +57,7 @@ const Homepage = () => {
   }, []);
   return (
     <>
-      <div className="banner">
+      <div className="container banner">
         <div className="banner_box">
           <Swipper
             swiperImage={swiperTopSlides.map((slide) => slide.image)}
@@ -70,148 +70,154 @@ const Homepage = () => {
           />
         </div>
       </div>
-      <div className="cp cp0 clearfix">
-        <div
-          className="cp_lt fl"
-          data-aos="slide-right"
-          data-aos-delay="300"
-          data-aos-duration="1000"
-          data-aos-easing="ease-out-back">
-          <img src={IndexProtop} />
-        </div>
-        <div className="cp_rt fr">
+      <div className="container cp cp0 clearfix">
+        <div className="row justify-content-between">
           <div
-            className="cp_rt1 clearfix"
-            data-aos="slide-left"
-            data-aos-easing="ease-out-back"
-            data-aos-duration="1000">
-            <div className="fl cp_rt_l">
-              <a href="product.html">
-                <img src={Tyre02} />
-              </a>
-            </div>
-            <div className="fr cp_rt_r">
-              <div className="cp_rt2">
-                <a href="product.html">Product</a>
-              </div>
-              <div className="cp_rt4">
-                Roadone is of the international high end quality gene and local
-                applicable advantage of Chinese Market. All the key equipment is
-                from the famous world-className tyre equipment suppliers. It
-                adopts the global leading technology of all steel radial truck
-                tyres from the European century producer. ...
-              </div>
-            </div>
+            className="col-6 cp_lt fl"
+            data-aos="slide-right"
+            data-aos-delay="300"
+            data-aos-duration="1000"
+            data-aos-easing="ease-out-back">
+            <img src={IndexProtop} />
           </div>
-          <div
-            className="cp_rt1 cp_rt2 clearfix"
-            data-aos="slide-up"
-            data-aos-easing="ease-out-back"
-            data-aos-duration="1000">
-            <div className="fr cp_rt_l">
-              <a href="marketing.html">
-                <img src={Tyre06} />
-              </a>
-            </div>
-            <div className="fr cp_rt_r cp_rt_r1">
-              <div className="cp_rt2 cp_rt5">
-                <a href="marketing.html">Marketing</a>
+          <div className="col-6 cp_rt fr">
+            <div
+              className="cp_rt1 clearfix d-flex"
+              data-aos="slide-left"
+              data-aos-easing="ease-out-back"
+              data-aos-duration="1000">
+              <div className="fl cp_rt_l">
+                <a href="product.html">
+                  <img src={Tyre02} />
+                </a>
               </div>
-              <div className="cp_rt4">
-                Roadone Stores provide not only the high quality truck and bus
-                tyres, but also good service for our customers. ...
+              <div className="fr cp_rt_r">
+                <div className="cp_rt2">
+                  <a href="product.html">Product</a>
+                </div>
+                <div className="cp_rt4">
+                  Roadone is of the international high end quality gene and
+                  local applicable advantage of Chinese Market. All the key
+                  equipment is from the famous world-className tyre equipment
+                  suppliers. It adopts the global leading technology of all
+                  steel radial truck tyres from the European century producer.
+                  ...
+                </div>
+              </div>
+            </div>
+            <div
+              className="cp_rt1 cp_rt2 clearfix d-flex"
+              data-aos="slide-up"
+              data-aos-easing="ease-out-back"
+              data-aos-duration="1000">
+              <div className="fr cp_rt_r cp_rt_r1">
+                <div className="cp_rt2 cp_rt5">
+                  <a href="marketing.html">Marketing</a>
+                </div>
+                <div className="cp_rt4">
+                  Roadone Stores provide not only the high quality truck and bus
+                  tyres, but also good service for our customers. ...
+                </div>
+              </div>{" "}
+              <div className="fr cp_rt_l">
+                <a href="marketing.html">
+                  <img src={Tyre06} />
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      <div className="cp cp2 clearfix">
-        <div
-          className="cp2_lt fl"
-          data-aos="slide-up"
-          data-aos-duration="1000"
-          data-aos-easing="ease-out-back">
-          <div className="news">News</div>
-          <div className="news_main">
-            <ul>
-              {newsItems.map((news, index) => (
-                <li key={index}>
-                  <div>{news.news_date}</div>
-                  <p>
-                    <a href={news.news_url} target="_blank">
-                      {news.news_title}
+      <div className="container cp cp2 clearfix">
+        <div className="row justify-content-between">
+          <div
+            className="col-6 cp2_lt fl"
+            data-aos="slide-up"
+            data-aos-duration="1000"
+            data-aos-easing="ease-out-back">
+            <div className="news">News</div>
+            <div className="news_main">
+              <ul>
+                {newsItems.map((news, index) => (
+                  <li key={index}>
+                    <div>{news.news_date}</div>
+                    <p>
+                      <a href={news.news_url} target="_blank">
+                        {news.news_title}
+                      </a>
+                    </p>
+                  </li>
+                ))}
+              </ul>
+              <div className="more">
+                <a href="news.html">MORE {">"}</a>
+              </div>
+            </div>
+          </div>
+          <div
+            className="col-6 fr"
+            data-aos="slide-left"
+            data-aos-delay="300"
+            data-aos-duration="1000"
+            data-aos-easing="ease-out-back">
+            <div className="cp2_rt_box">
+              <Swiper
+                pagination={{ clickable: true }}
+                loop={true}
+                className="swiper-container"
+                id="banner">
+                {swiperSlides.map((slide, index) => (
+                  <SwiperSlide key={index}>
+                    <a>
+                      <img src={slide.image} />
                     </a>
-                  </p>
-                </li>
-              ))}
-            </ul>
-            <div className="more">
-              <a href="news.html">MORE {">"}</a>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
             </div>
           </div>
         </div>
-        <div
-          className="cp2_rt fr"
-          data-aos="slide-left"
-          data-aos-delay="300"
-          data-aos-duration="1000"
-          data-aos-easing="ease-out-back">
-          <div className="cp2_rt_box">
-            <Swiper
-              pagination={{ clickable: true }}
-              loop={true}
-              className="swiper-container"
-              id="banner">
-              {swiperSlides.map((slide, index) => (
-                <SwiperSlide key={index}>
-                  <a>
-                    <img src={slide.image} />
-                  </a>
-                </SwiperSlide>
-              ))}
-            </Swiper>
+        <div className="row">
+          <div
+            className="cp cp3 aos-init aos-animate"
+            data-aos="slide-up"
+            data-aos-duration="1000"
+            data-aos-easing="ease-out-back">
+            <img src={BotImage} />
           </div>
         </div>
-
-        <div
-          className="cp cp3 aos-init aos-animate"
-          data-aos="slide-up"
-          data-aos-duration="1000"
-          data-aos-easing="ease-out-back">
-          <img src={BotImage} />
-        </div>
-
-        <div
-          className="cp cp4 clearfix aos-init"
-          data-aos-duration="1000"
-          data-aos="slide-up"
-          data-aos-easing="ease-out-back">
-          <div className="cp4_box">
-            <div className="ul2 fl">
-              <ul>
-                <li className="first_li">Tongli Tyre Co., Ltd.</li>
-                <li>
-                  Hixih Industrial Park, Yanzhou District, Jining City, Shandong
-                  Province, China. 272199
-                </li>
-                <li>
-                  <span>
-                    National Service Hotline: <s>400-639-1111</s>
-                  </span>
-                  <i>
-                    International Service Hotline: <s>+86-537-5174476</s>
-                  </i>
-                </li>
-                <li>
-                  <span>
-                    E-mail: <s>roadone@hixih.com.cn</s>{" "}
-                  </span>
-                  <i className="web">
-                    Website: <s>http://en.tltyre.com</s>
-                  </i>
-                </li>
-              </ul>
+        <div className="row">
+          <div
+            className="cp cp4 clearfix aos-init"
+            data-aos-duration="1000"
+            data-aos="slide-up"
+            data-aos-easing="ease-out-back">
+            <div className="cp4_box">
+              <div className="ul2 fl">
+                <ul>
+                  <li className="first_li">Tongli Tyre Co., Ltd.</li>
+                  <li>
+                    Hixih Industrial Park, Yanzhou District, Jining City,
+                    Shandong Province, China. 272199
+                  </li>
+                  <li>
+                    <span>
+                      National Service Hotline: <s>400-639-1111</s>
+                    </span>
+                    <i>
+                      International Service Hotline: <s>+86-537-5174476</s>
+                    </i>
+                  </li>
+                  <li>
+                    <span>
+                      E-mail: <s>roadone@hixih.com.cn</s>{" "}
+                    </span>
+                    <i className="web">
+                      Website: <s>http://en.tltyre.com</s>
+                    </i>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
