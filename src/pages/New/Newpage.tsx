6e45b0b5fc1935be18d6c28f4273dc4687e00505
weikang <weikang.ng@evdg.cc>
2023-08-25 17:16:24 +0800

    update

--- src/pages/New/Newpage.tsx
+++ src/pages/New/Newpage.tsx
@@ -1,23 +1,15 @@
 import React, { useEffect, useState } from "react";
-import Header from "../../component/Header/Header";
+import { useTranslation } from "react-i18next";
 
-import LogoOther from "../../static/assets/main/cd_logo.png";
 import NewsDateIcon from "../../static/assets/picture/news_date.png";
 import axios from "axios";
-import Header_m from "../../component/Header/Header_m";
-import Swipper_m from "../../component/Swiper/Swipper_m";
-import NewBanner from "../../static/assets/m/xw_banner.png";
 import NewHeader from "./NewHeader";
 
 const New = () => {
+  const { t } = useTranslation();
+
   const [newsItems, setNewsItems] = useState([]);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const swiperTopMobileSlides = [{ image: NewBanner }];
-  const [clickedProductId, setClickedProductId] = useState(null);
-
-  const resetClickedProductId = () => {
-    setClickedProductId(null);
-  };
 
   useEffect(() => {
     // Fetch data from phpMyAdmin using Axios
@@ -52,34 +44,42 @@ const New = () => {
         </div>
         <div className="news_box">
           <ul>
-            {newsItems.map((news, index) => (
-              <li key={index}>
-                <div className="news_m clearfix">
-                  <div className="col-lg-4">
-                    <a href={news.news_url} className="news_img">
-                      <img
-                        src={require(`../../static/assets/picture/${news.news_image}`)}
-                      />
-                    </a>
-                  </div>
-                  <div className="news_rt fr ps-4">
-                    <div className="news_rt_d1"></div>
-                    <div className="news_rt_d2">{news.news_content}</div>
-                    <div className="news_rt_d3 clearfix">
-                      <p className="fl">
-                        <img src={NewsDateIcon} alt="News Date" />
-                        <span className="news_date">{news.news_date}</span>
-                      </p>
-                      <div className="news_rt_d3r fr clearfix">
-                        <div className="yuedu fl">
-                          <a>View details</a>
+            {newsItems.map((news, index) => {
+              const translationIndex = parseInt(news.id, 10);
+              return (
+                <li key={index}>
+                  <div className="news_m clearfix">
+                    <div className="col-lg-4">
+                      <a href={news.news_url} className="news_img">
+                        <img
+                          src={require(`../../static/assets/picture/${news.news_image}`)}
+                        />
+                      </a>
+                    </div>
+                    <div className="news_rt fr ps-4">
+                      <div className="news_rt_d1">
+                        {" "}
+                        <a> {t(`news.news_title.${translationIndex}`)}</a>
+                      </div>
+                      <div className="news_rt_d2">
+                        {t(`news.news_content.${translationIndex}`)}
+                      </div>
+                      <div className="news_rt_d3 clearfix">
+                        <p className="fl">
+                          <img src={NewsDateIcon} alt="News Date" />
+                          <span className="news_date">{news.news_date}</span>
+                        </p>
+                        <div className="news_rt_d3r fr clearfix">
+                          <div className="yuedu fl">
+                            <a>{t("homepage.viewDetails")}</a>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </li>
-            ))}
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
